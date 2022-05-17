03195aed6757325c99c37efe5aa01f3ae3fabb8a
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-05-17 19:32:42 +0100

    Project2 was created

--- Project1/cmake-build-debug/cmake_install.cmake
+++ Project1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/magalhaesm3/Documents/Mafalda/FEUP/DA/Projects/DA_Project
+# Install script for directory: /Users/magalhaesm3/Documents/Mafalda/FEUP/Projects/DA/DA_Projects/Project1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/magalhaesm3/Documents/Mafalda/FEUP/DA/Projects/DA_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/magalhaesm3/Documents/Mafalda/FEUP/Projects/DA/DA_Projects/Project1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ Project2/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Mafalda Magalhães on 17/05/2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
